91542d62d6e89781effc3659809333ff20b7f2a2
Nebojsa Lucic <nebojsa.lucic@gmail.com>
2019-07-22 22:02:28 +0200

    ubaceni entiteti i enumeracije

--- src/main/java/com/iktpreobuka/ednevnik/entities/UserEntity.java
+++ src/main/java/com/iktpreobuka/ednevnik/entities/UserEntity.java
@@ -1,8 +1,13 @@
 package com.iktpreobuka.ednevnik.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import com.iktpreobuka.ednevnik.enumerations.EUserRole;
 
+@Entity
 public class UserEntity {
+	@Id
 	private Integer userId;
 	private String username;
 	private String password;
